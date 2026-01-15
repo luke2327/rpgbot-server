1ce3f84a9b56196b22bd336b5b9e3a17147c69fd
luke2327 <qwepoi8526@gmail.com>
2026-01-15 10:28:19 +0900

    add: doc 추가

--- src/libs/kakao.utils.ts
+++ src/libs/kakao.utils.ts
@@ -1,16 +1,66 @@
 export const kakaoTemplate = {
-  simpleText: (data: string) => {
+  /**
+   *
+   * @param text
+   * {@link https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format#example_code-6}
+   */
+  simpleText: (text: string) => {
     return {
       version: '2.0',
       template: {
         outputs: [
           {
             simpleText: {
-              text: data,
+              text: text,
             },
           },
         ],
       },
     }
   },
+  /**
+   *
+   * @param textCard
+   * {@link https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format#example_code-8}
+   */
+  textCard: (textCard: TextCard) => {
+    return {
+      version: '2.0',
+      template: {
+        outputs: [{ textCard }],
+      },
+    }
+  },
+  /**
+   *
+   * @param basicCard
+   * {@link https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format#example_code-9}
+   */
+  basicCard: (basicCard: BasicCard) => {
+    return {
+      version: '2.0',
+      template: {
+        outputs: [{ basicCard }],
+      },
+    }
+  },
+}
+
+type BasicCard = {
+  title: string
+  description: string
+  thumbnail: string
+  buttons: CardButton[]
+}
+
+type TextCard = {
+  title: string
+  description: string
+  buttons: CardButton[]
+}
+
+type CardButton = {
+  action: string
+  label: string
+  webLinkUrl: string
 }
