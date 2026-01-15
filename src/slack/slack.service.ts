9597b11a368bef1265ac381b16c713bdae6f25dd
luke2327 <qwepoi8526@gmail.com>
2026-01-15 14:47:41 +0900

    add: slack service

---
+++ src/slack/slack.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+import { WebClient } from '@slack/web-api'
+import { env } from '@/configs/env'
+
+@Injectable()
+export class SlackService {
+  public web: WebClient
+
+  constructor() {
+    this.web = new WebClient(env.SLACK_TOKEN)
+  }
+}
